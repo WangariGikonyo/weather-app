92a02c7ed00e57a5386b601f7c16f38110949365
Something Inc <KareWangari>
2019-07-04 13:38:33 +0300

    edited weather component and added an icon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LocationForm from './components/LocationForm';
 import Weather from './components/Weather';
 import './App.css';
 
-const API_KEY = 'f34f1a4ac483a6f4eaa262533c01847f';
+const API_KEY = 'c9ff504cde8549e886991433190407 ';
 class App extends React.Component{
   state={
     temperature:undefined,
@@ -12,22 +12,27 @@ class App extends React.Component{
     country:undefined,
     humidity:undefined,
     description:undefined,
-    error:undefined
+    icon:undefined,
+    error:undefined,
+    day:undefined,
   }
   getWeather = async(e)=>{
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+
+    const api = await fetch(`http://api.apixu.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=6
+`);
     const data = await api.json();
     if(city && country){
       console.log(data);
       this.setState({
-        temperature:data.main.temp,
-        city:data.name,
-        country:data.sys.country,
-        humidity:data.main.humidity,
-        description:data.weather[0].description,
+        temperature:data.current.temp_c,
+        city:data.location.name,
+        country:data.location.country,
+        description:data.current.condition.text,
+        day:data.current.is_day,
+        icon:data.current.condition.icon,
         error:''
       })
     }else{
@@ -56,6 +61,7 @@ class App extends React.Component{
           country = {this.state.country}
           humidity={this.state.humidity}
           description={this.state.description}
+          icon={this.state.icon}
           error={this.state.error}
         />
       </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,13 @@ const Weather = (props)=>{
     return(
       <div>
       {props.city && props.country && <p>Location:{props.country},{props.city}</p>}
-      {props.temperature && <p>temperature:{props.temperature}</p>}
+      {props.temperature && <p>temperature:{props.temperature}°</p>}
       {props.humidity && <p>Humidity:{props.humidity}</p>}
       {props.description && <p>Situation:{props.description}</p>}
+      {props.icon && <img src={props.icon} />}
       {props.error && <p>{props.error}</p>}
+
+      {props.day && <p>{props.day}</p>}
       </div>
     );
 
